4811cbf657222828816330ae215d1eb955e922dd
rizkyfebriann <rizkyyfebriann@gmail.com>
2022-08-15 21:39:05 +0700

    create each class middlemanAPI

--- src/test/java/Middleman/API/MiddlemanAPI_InOutBounds.java
+++ src/test/java/Middleman/API/MiddlemanAPI_InOutBounds.java
@@ -6,16 +6,11 @@ import net.thucydides.core.annotations.Step;
 public class MiddlemanAPI_InOutBounds {
     public static final String URL = "https://postme.site";
     public static final String DIR = System.getProperty("user.dir");
-
     public static String TOKEN_RIZKYUSER = "";
     public static String TOKEN_RIZKYUSER1 = "";
     public static String GET_ALL_CART_INOUTBOUNDS = URL+"/inoutbounds";
 
     public static String GET_ALL_CART_INVALID_INOUTBOUNDS = URL+"/inoutbound";
-
-
-
-
 
     @Step("Get all carts on inoutbounds")
     public static void getAllCartInoutboundsUser() {
