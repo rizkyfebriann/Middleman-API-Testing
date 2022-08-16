32cc2f257cbac70a0d7bbb413b3ae9b7f25c2d18
rizkyfebriann <rizkyyfebriann@gmail.com>
2022-08-16 21:36:22 +0700

    added more scenario for inoutbounds feature

--- src/test/java/Middleman/HooksLogin1.java
+++ src/test/java/Middleman/HooksLogin1.java
@@ -13,21 +13,11 @@ import java.io.File;
 
 import static Middleman.API.MiddlemanAPI_InOutBounds.URL;
 
-public class Hooks {
-    @Before("@loginAdmins")
-    public void login_admins(){
-        File json = new File("src/test/resources/JSONFile/Login/ValidLoginAdmin.json");
-        Response response= SerenityRest.given()
-                .contentType(ContentType.JSON)
-                .body(json)
-                .post(URL+"/login");
-        JsonPath jsonPath = response.jsonPath();
-        MiddlemanAPI_Admins.TOKEN_ADMINS = "Bearer "+ jsonPath.get("data.token");
-    }
+public class HooksLogin1 {
 
     @Before("@InvalidToken")
     public void invalidToken(){
-        MiddlemanAPI_Admins.TOKEN_ADMINS = "Bearer invalid1234567io";
+        MiddlemanAPI_InOutBounds.TOKEN_ADMIN_INVALID = "Bearer invalid1234567io";
     }
 
     @Before("@loginRizkyUser")
@@ -43,22 +33,28 @@ public class Hooks {
 
     @Before("@loginRizkyUser1")
     public void loginRizkyUser1(){
-        File json = new File("src/test/resources/JSONFile/Login/ValidLoginUser2.json");
+        File json = new File("src/test/resources/JSONFile/Login/ValidLoginUser1.json");
         Response response= SerenityRest.given()
                 .contentType(ContentType.JSON)
                 .body(json)
                 .post(URL+"/login");
         JsonPath jsonPath = response.jsonPath();
-        MiddlemanAPI_InOutBounds.TOKEN_RIZKYUSER = jsonPath.get("data.token");
+        MiddlemanAPI_InOutBounds.TOKEN_RIZKYUSER1 = jsonPath.get("data.token");
+    }
+
+    @Before("@loginAdmins")
+    public void loginAdmin(){
+        File json = new File("src/test/resources/JSONFile/Login/ValidLoginAdmin.json");
+        Response response= SerenityRest.given()
+                .contentType(ContentType.JSON)
+                .body(json)
+                .post(URL+"/login");
+        JsonPath jsonPath = response.jsonPath();
+        MiddlemanAPI_InOutBounds.TOKEN_ADMIN = jsonPath.get("data.token");
     }
 
     @After("@loginRizkyUser")
-    public void reset_token_rizkyUser(){
+    public void reset_token_User(){
         MiddlemanAPI_InOutBounds.TOKEN_RIZKYUSER = "";
-    }
-
-    @After("@loginAdmins")
-    public void reset_token_admins(){
-        MiddlemanAPI_Admins.TOKEN_ADMINS = "x";
     }
 }
